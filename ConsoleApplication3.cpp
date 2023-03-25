c44b6ed2e909115db64fb29aa4db4d4682047785
Markov146 <denismarkov954@gmail.com>
2023-03-25 10:32:57 +0300

    Загружаю свой проект

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+class Person
+{
+    std::string name;
+    int age;
+};
+
+int main()
+{
+    Person p{"Alex",18};
+    nlohmann::json j{};
+    j["name"] = p.name;
+    j["age"] = p.age;
+}
+
